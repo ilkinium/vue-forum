a49ab0ec69bb1be98135a3559929e74e363226b7
Ilkin Fleydanli <i.fleydanli@gmail.com>
2020-01-28 18:24:19 +0400

    Initial commit

---
+++ src/pages/NotFoundPage.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="col-full" :class="$style['content-centered']">
+    <h1 :class="$style['heading-large']">Not Found</h1>
+    <p :class="$style['text-large']">
+      Oops, page not found!
+      <router-link :to="{name: 'Home'}">
+        Go To Home Page
+      </router-link>
+    </p>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'NotFound'
+  }
+</script>
+
+<style module>
+  .content-centered {
+    text-align: center;
+    padding: 5em;
+  }
+
+  .heading-large {
+    font-size: 5em;
+  }
+
+  .text-large {
+    font-size: 2.5em;
+    font-weight: lighter;
+  }
+</style>
---
+++ src/router/index.js
@@ -0,0 +1,29 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import ThreadShow from '../pages/ThreadShowPage'
+import Home from '../pages/HomePage'
+import NotFound from '../pages/NotFoundPage'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'Home',
+      component: Home
+    },
+    {
+      path: '/thread/:id',
+      name: 'ThreadShow',
+      component: ThreadShow,
+      props: true
+    },
+    {
+      path: '*',
+      name: 'NotFound',
+      component: NotFound
+    }
+  ],
+  mode: 'history'
+})
