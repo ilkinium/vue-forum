7a8dd1e1f02d5020018ff26892c703f34f8a178d
Ilkin Fleydanli <i.fleydanli@gmail.com>
2020-01-28 19:21:15 +0400

    added PostEditor component
    added post Editor component,
    changes in threadshowPage after creating new component

---
+++ src/components/PostEditor.vue
@@ -0,0 +1,45 @@
+<template>
+  <form @submit.prevent="save">
+    <div class="form-group">
+      <textarea name="" id="" cols="30" rows="10" class="form-input" v-model="newPostText"></textarea>
+    </div>
+    <div class="form-actions">
+      <button class="btn-blue">submit</button>
+    </div>
+  </form>
+</template>
+
+<script>
+  export default {
+    name: 'PostEditor',
+    props: {
+      threadId: {
+        required: true
+      }
+    },
+    data () {
+      return {
+        newPostText: ''
+      }
+    },
+    methods: {
+      save () {
+        const postId = 'postID-' + Math.random()
+        const post = {
+          text: this.newPostText,
+          publishedAt: Math.floor(Date.now() / 1000),
+          threadId: this.threadId,
+          userId: 'L664y3qZSubDbT1R6npC0EEybJ73',
+          '.key': postId
+        }
+        this.newPostText = ''
+
+        this.$emit('save-post', {post})
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
--- src/pages/ThreadShowPage.vue
+++ src/pages/ThreadShowPage.vue
@@ -2,16 +2,20 @@
   <div class="col-large push-top">
     <h1>{{thread.title}}</h1>
     <PostList :posts="posts"/>
+    <PostEditor @save-post="addPost" :threadId="id"></PostEditor>
   </div>
 </template>
 
 <script>
   import sourceData from '@/data'
   import PostList from '@/components/PostList'
+  import PostEditor from '../components/PostEditor'
+
   export default {
     name: 'ThreadShowPage',
     components: {
-      PostList
+      PostList,
+      PostEditor
     },
     props: {
       id: {
@@ -21,7 +25,18 @@
     },
     data () {
       return {
-        thread: sourceData.threads[this.id]
+        thread: sourceData.threads[this.id],
+        newPostText: ''
+      }
+    },
+    methods: {
+      addPost (event) {
+        // console.log(event)
+        const post = event.post
+        const postId = event.post['.key']
+        this.$set(sourceData.posts, postId, post)
+        this.$set(this.thread.posts, postId, postId)
+        this.$set(sourceData.users[post.userId].posts, postId, postId)
       }
     },
     computed: {
